bf016004fccb165ca6b02f75ab847bda5a089302
“Ana <teamurjc@gmail.com>
2024-02-21 19:22:02 +0100

    data processing MVP & streamlit 21.02

--- modules/acquisition.py
+++ modules/acquisition.py
@@ -18,34 +18,44 @@ m_pilas_marquesinas = pd.read_csv('../data/Madrid/Marquesinas_contenedores_pilas
 #Puntos limpios fijos
 m_puntos_limpios_fijos = m_puntos_limpios_fijos.rename(columns={'LATITUD': 'LATITUDE'})
 m_puntos_limpios_fijos = m_puntos_limpios_fijos.rename(columns={'LONGITUD': 'LONGITUDE'})
+m_puntos_limpios_fijos['DIRECTIONS'] = m_puntos_limpios_fijos['NOMBRE-VIA'].astype(str) + ' '+ m_puntos_limpios_fijos['NUM'].astype(str)
 #Ropa
 m_ropa = m_ropa.rename(columns={'LATITUD': 'LATITUDE'})
 m_ropa = m_ropa.rename(columns={'LONGITUD': 'LONGITUDE'})
 m_ropa['LATITUDE'] = pd.to_numeric(m_ropa['LATITUDE'].str.replace(',', '.'))
 m_ropa['LONGITUDE'] = pd.to_numeric(m_ropa['LONGITUDE'].str.replace(',', '.'))
+m_ropa = m_ropa.rename(columns={'DIRECCION_COMPLETA': 'DIRECTIONS'})
 #Resto
 m_resto = m_resto.rename(columns={'LATITUD': 'LATITUDE'})
 m_resto = m_resto.rename(columns={'LONGITUD': 'LONGITUDE'})
+m_resto = m_resto.rename(columns={'DIRECCION': 'DIRECTIONS'})
 #Envases
 m_envases = m_envases.rename(columns={'LATITUD': 'LATITUDE'})
 m_envases = m_envases.rename(columns={'LONGITUD': 'LONGITUDE'})
+m_envases = m_envases.rename(columns={'DIRECCION': 'DIRECTIONS'})
 #Papel
 m_papel = m_papel.rename(columns={'LATITUD': 'LATITUDE'})
 m_papel = m_papel.rename(columns={'LONGITUD': 'LONGITUDE'})
+m_papel = m_papel.rename(columns={'DIRECCION': 'DIRECTIONS'})
 #Vidrio
 m_vidrio = m_vidrio.rename(columns={'LATITUD': 'LATITUDE'})
 m_vidrio = m_vidrio.rename(columns={'LONGITUD': 'LONGITUDE'})
+m_vidrio = m_vidrio.rename(columns={'DIRECCION': 'DIRECTIONS'})
 #Organica
 m_organica = m_organica.rename(columns={'LATITUD': 'LATITUDE'})
 m_organica = m_organica.rename(columns={'LONGITUD': 'LONGITUDE'})
+m_organica = m_organica.rename(columns={'DIRECCION': 'DIRECTIONS'})
 #Pilas
 m_pilas_marquesinas = m_pilas_marquesinas.rename(columns={'Latitud': 'LATITUDE'})
 m_pilas_marquesinas = m_pilas_marquesinas.rename(columns={'Longitud': 'LONGITUDE'})
+m_pilas_marquesinas = m_pilas_marquesinas.rename(columns={'Direccion_completa': 'DIRECTIONS'})
 #Puntos limpios móviles
 m_puntos_limpios_moviles = m_puntos_limpios_moviles.rename(columns={'LATITUD': 'LATITUDE'})
 m_puntos_limpios_moviles = m_puntos_limpios_moviles.rename(columns={'LONGITUD': 'LONGITUDE'})
+m_puntos_limpios_moviles = m_puntos_limpios_moviles.rename(columns={'DIRECCION_COMPLETA': 'DIRECTIONS'})
 #Aceite
 m_aceite = m_aceite.rename(columns={'LATITUD': 'LATITUDE'})
 m_aceite = m_aceite.rename(columns={'LONGITUD': 'LONGITUDE'})
+m_aceite = m_aceite.rename(columns={'DIRECCION COMPLETA': 'DIRECTIONS'})
 m_aceite['LATITUDE'] = pd.to_numeric(m_aceite['LATITUDE'].str.replace(',', '.'))
 m_aceite['LONGITUDE'] = pd.to_numeric(m_aceite['LONGITUDE'].str.replace(',', '.'))
--- modules/app.py
+++ modules/app.py
@@ -9,7 +9,7 @@ from acquisition import m_organica as organica
 from acquisition import m_pilas_marquesinas as pilas
 from acquisition import m_puntos_limpios_moviles as moviles
 from acquisition import m_aceite as aceite
-
+import pydeck as pdk
 import pandas as pd
 
 st.title('Gestión de residuos Madrid')
@@ -29,12 +29,58 @@ dataframes_map = {
     'Aceite': aceite 
 }
 
+color_map = {
+    'Aceite': [255, 165, 0],  # Orange
+    'Resto': [255, 0, 0],  # Red
+    'Orgánica': [165, 42, 42],  # Brown
+    'Papel': [0, 0, 255],  # Blue
+    'Envases': [255, 255, 0],  # Yellow
+    'Vidrio': [0, 128, 0],  # Green
+    'Pilas': [160, 32, 240],  # Purple
+    'Ropa': [255, 192, 203],  # Pink
+    'Varios': [128, 128, 128]  # Grey for combined categories
+}
+
+standard_size = 50
 
 if bin_type in dataframes_map:
-    st.map(dataframes_map[bin_type][['LATITUDE', 'LONGITUDE']])
+     df_to_display = dataframes_map[bin_type]
 elif bin_type == 'Varios':
-    combined_df = pd.concat([fijos, moviles])
-    st.map(combined_df[['LATITUDE', 'LONGITUDE']])
+    # Combine DataFrames for 'Varios'
+    df_to_display = pd.concat([fijos, moviles])
 else:
-
     st.write("Selected bin type is not recognized.")
+    df_to_display = pd.DataFrame()  # Empty DataFrame to prevent errors
+
+# Ensure the DataFrame to display is not empty
+if not df_to_display.empty:
+    # Convert LATITUDE and LONGITUDE to float if they are not already
+    df_to_display['LATITUDE'] = pd.to_numeric(df_to_display['LATITUDE'], errors='coerce')
+    df_to_display['LONGITUDE'] = pd.to_numeric(df_to_display['LONGITUDE'], errors='coerce')
+    
+    # Create the PyDeck layer
+    layer = pdk.Layer(
+        'ScatterplotLayer',
+        df_to_display,
+        get_position='[LONGITUDE, LATITUDE]',
+        get_color=color_map[bin_type],
+        get_radius=standard_size,
+        pickable=True,
+        auto_highlight=True,
+    )
+    tooltip = {
+        "html": "{DIRECTIONS}",
+        "style": {
+            "backgroundColor": "steelblue",
+            "color": "white", 
+            "fontSize": "10px",  
+            "padding": "3px", 
+            "maxWidth": "100px"
+        }
+    }
+    # Set the view
+    view_state = pdk.ViewState(latitude=df_to_display['LATITUDE'].mean(), longitude=df_to_display['LONGITUDE'].mean(), zoom=11)
+
+    # Render the map
+    r = pdk.Deck(layers=[layer], initial_view_state=view_state, map_style='mapbox://styles/mapbox/light-v9',tooltip=tooltip)
+    st.pydeck_chart(r)
