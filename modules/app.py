88777693310cbfdd42edf03a67d54c0a9780d941
“Ana <teamurjc@gmail.com>
2024-02-24 13:33:18 +0100

    API & streamlit 24.02 v2

--- modules/app.py
+++ modules/app.py
@@ -1,3 +1,4 @@
+import requests
 import streamlit as st
 from acquisition import m_puntos_limpios_fijos as fijos
 from acquisition import m_ropa as ropa
@@ -17,6 +18,8 @@ from math import radians
 import time 
 from geopy.distance import geodesic
 from api import client as api
+import os
+
 
 st.title('Gestión de residuos Madrid')
 
@@ -74,6 +77,7 @@ if user_address:
             user_lat, user_lon = location.latitude, location.longitude
             df_to_display['LATITUDE'] = pd.to_numeric(df_to_display['LATITUDE'], errors='coerce')
             df_to_display['LONGITUDE'] = pd.to_numeric(df_to_display['LONGITUDE'], errors='coerce')
+            df_to_display.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
 
             # Calculate distances and find the nearest location
             df_to_display['DISTANCE'] = df_to_display.apply(lambda row: geodesic((user_lat, user_lon), (row['LATITUDE'], row['LONGITUDE'])).km, axis=1)
@@ -89,7 +93,35 @@ if user_address:
                     st.write(f"El punto limpio más cercano para {bin_type} está en: {nearest_location['DIRECTIONS']}, a {nearest_location['DISTANCE']:.2f} km de distancia.")
             else:
                 st.write(f"El contenedor de {bin_type} más cercano está en: {nearest_location['DIRECTIONS']}, a {nearest_location['DISTANCE']:.2f} km de distancia.")
-
+            
+            def get_route(start_lat, start_lon, end_lat, end_lon, api_key):
+                url = f"https://api.openrouteservice.org/v2/directions/driving-car"
+                api_key = os.getenv('token')
+                headers = {"Authorization": api_key}
+                params = {
+                    "start": f"{start_lon},{start_lat}",
+                    "end": f"{end_lon},{end_lat}"
+                }
+                response = requests.get(url, headers=headers, params=params)
+                if response.status_code == 200:
+                    return response.json()
+                else:
+                    return None
+            route = get_route(user_lat, user_lon, nearest_location['LATITUDE'], nearest_location['LONGITUDE'], api)
+            if route:
+                # Extracting the geometry from the route to display on the map
+                route_geometry = route['features'][0]['geometry']
+                route_layer = pdk.Layer(
+                    'PathLayer',
+                    data=pd.DataFrame({"path": [route_geometry['coordinates']]}),
+                    get_path="path",
+                    width_scale=20,
+                    width_min_pixels=2,
+                    get_color=[255, 100, 100],
+                    pickable=True,
+                )
+            else:
+                st.error("No se pudo obtener la ruta. Asegúrate de que tu API key es válida y tienes acceso a Internet.")
             # Map setup
             all_locations_layer = pdk.Layer(
                 'ScatterplotLayer',
@@ -105,7 +137,7 @@ if user_address:
                 nearest_df,
                 get_position='[LONGITUDE, LATITUDE]',
                 get_color=[0, 33, 243],  # Highlight color
-                get_radius=80,  # Larger to highlight
+                get_radius=50,  # Larger to highlight
                 pickable=True,
                 auto_highlight=True
             )
@@ -120,7 +152,12 @@ if user_address:
                 }
             }
             view_state = pdk.ViewState(latitude=user_lat, longitude=user_lon, zoom=13)
-            r = pdk.Deck(layers=[all_locations_layer, nearest_location_layer], initial_view_state=view_state, map_style='mapbox://styles/mapbox/light-v9',tooltip=tooltip)
+            r = pdk.Deck(
+                layers=[all_locations_layer, nearest_location_layer, route_layer],  # Include route_layer here
+                initial_view_state=pdk.ViewState(latitude=user_lat, longitude=user_lon, zoom=13),
+                map_style='mapbox://styles/mapbox/light-v9',
+                tooltip=tooltip
+            )
             st.pydeck_chart(r)
         else:
             st.error("No se pudo identificar la ubicación. Intenta con una dirección más específica.")
